58f3ec57b1955f81da1ed5fd964ee73f45a86f42
meo-young <meoyoung000@naver.com>
2024-12-02 23:18:19 +0900

    School_Main 씬 3-1 Layer 설정

---
+++ Assets/02. Script/Layer/LayerSetting.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class LayerSetting : MonoBehaviour
+{
+    private SpriteRenderer targetRenderer;
+    private SpriteRenderer playerRenderer;
+    void Start()
+    {
+        targetRenderer = GetComponent<SpriteRenderer>();
+        playerRenderer = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (MainController.instance.movement.y < 0)
+            targetRenderer.sortingOrder = playerRenderer.sortingOrder - 3;
+        else if (MainController.instance.movement.y > 0)
+            targetRenderer.sortingOrder = playerRenderer.sortingOrder + 3;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (MainController.instance.movement.y < 0)
+            targetRenderer.sortingOrder = playerRenderer.sortingOrder - 3;
+        else if (MainController.instance.movement.y > 0)
+            targetRenderer.sortingOrder = playerRenderer.sortingOrder + 3;
+
+    }
+}
