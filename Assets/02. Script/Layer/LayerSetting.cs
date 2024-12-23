a2c9895d678bf949dc1cf241f6e13d0a336104a7
meo-young <meoyoung000@naver.com>
2024-12-23 23:44:48 +0900

    3-1 BoxCollider 세팅 끝

--- Assets/02. Script/Layer/LayerSetting.cs
+++ Assets/02. Script/Layer/LayerSetting.cs
@@ -4,10 +4,15 @@ public class LayerSetting : MonoBehaviour
 {
     private SpriteRenderer targetRenderer;
     private SpriteRenderer playerRenderer;
+
+    private int originSortingOrder;
+
     void Start()
     {
         targetRenderer = GetComponentInParent<SpriteRenderer>();
         playerRenderer = GameManager.instance.player.GetComponent<SpriteRenderer>();
+
+        originSortingOrder = targetRenderer.sortingOrder;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -16,9 +21,9 @@ public class LayerSetting : MonoBehaviour
             return;
 
         if (MainController.instance.movement.y < 0)
-            targetRenderer.sortingOrder = playerRenderer.sortingOrder - 3;
+            targetRenderer.sortingOrder = originSortingOrder - playerRenderer.sortingOrder;
         else if (MainController.instance.movement.y > 0)
-            targetRenderer.sortingOrder = playerRenderer.sortingOrder + 3;
+            targetRenderer.sortingOrder = originSortingOrder + playerRenderer.sortingOrder;
     }
 
     void OnTriggerExit2D(Collider2D other)
@@ -27,9 +32,9 @@ public class LayerSetting : MonoBehaviour
             return;
 
         if (MainController.instance.movement.y < 0)
-            targetRenderer.sortingOrder = playerRenderer.sortingOrder - 3;
+            targetRenderer.sortingOrder = originSortingOrder - playerRenderer.sortingOrder;
         else if (MainController.instance.movement.y > 0)
-            targetRenderer.sortingOrder = playerRenderer.sortingOrder + 3;
+            targetRenderer.sortingOrder = originSortingOrder + playerRenderer.sortingOrder;
 
     }
 }
