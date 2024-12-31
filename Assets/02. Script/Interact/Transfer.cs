717e793096c030b8ad4ce264d5a3400f78d1571b
meo-young <meoyoung000@naver.com>
2024-12-31 15:27:15 +0900

    Transfer 기능 구현

---
+++ Assets/02. Script/Interact/Transfer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Transfer : MonoBehaviour
+{
+    [SerializeField] Direction direction;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Vector3 pos = collision.transform.position;
+            switch (direction)
+            {
+                case Direction.UP:
+                    pos.y += 6f;
+                    collision.transform.position = pos;
+                    break;
+                case Direction.DOWN:
+                    pos.y -= 6f;
+                    collision.transform.position = pos;
+                    break;
+                case Direction.LEFT:
+                    pos.x -= 6f;
+                    collision.transform.position = pos;
+                    break;
+                case Direction.RIGHT:
+                    pos.x += 6f;
+                    collision.transform.position = pos;
+                    break;
+            }
+        }
+    }
+
+    [System.Serializable]
+    public enum Direction
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+}
