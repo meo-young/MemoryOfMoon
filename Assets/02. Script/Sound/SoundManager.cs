0aa653fd1bf2ec78f5f01c82a39592f564e677b2
meo-young <meoyoung000@naver.com>
2025-01-22 15:21:28 +0900

    Add SerializedCollection
    - Dictionary의 직렬화를 위해 에셋 도입

---
+++ Assets/02. Script/Sound/SoundManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+}
