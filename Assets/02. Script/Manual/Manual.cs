f6bb4583c6e90dc00af7c7af4ded5674562c65f6
meo-young <meoyoung000@naver.com>
2025-01-22 15:53:55 +0900

    Update Manual Scene
    - Chapter1에서 Manual 출력 부분을 씬으로 분리

--- Assets/02. Script/Manual/Manual.cs
+++ Assets/02. Script/Manual/Manual.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using static Constant;
 
 public class Manual : MonoBehaviour
 {
     private Image manual;
+    private TMP_Text keepGoing;
     private bool canClick = false;
 
     private void Awake() {
         manual = GetComponentInChildren<Image>();
+        keepGoing = GetComponentInChildren<TMP_Text>();
+        keepGoing.gameObject.SetActive(false);
     }
 
     private void Start() {
         FadeManager2.instance.FadeOut(
             image : manual,
-            onComplete : () => canClick = true
+            onComplete : () => 
+            {   
+                canClick = true;
+                keepGoing.gameObject.SetActive(true);
+            }
         );
     }
 
