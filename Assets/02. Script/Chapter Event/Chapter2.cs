c0ba15be581edf6c7104836ae2d1e9ce9e407bdc
meo-young <meoyoung000@naver.com>
2025-02-15 20:41:23 +0900

    Update

--- Assets/02. Script/Chapter Event/Chapter2.cs
+++ Assets/02. Script/Chapter Event/Chapter2.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections;
 using UnityEngine.Playables;
+using AYellowpaper.SerializedCollections;
+
 
 public class Chapter2 : MonoBehaviour
 {
@@ -9,6 +11,15 @@ public class Chapter2 : MonoBehaviour
     [SerializeField] PlayableDirector[] timeline;
 
     private int currentTimelineIndex = 0;
+
+    private void Awake() {
+        foreach (var item in timeline)
+        {
+            item.stopped += (pd) => {
+                Destroy(pd.gameObject);
+            };
+        }
+    }
     
     public void ChapterTimeline()
     {
@@ -18,17 +29,28 @@ public class Chapter2 : MonoBehaviour
     public void NextTimeline()
     {
         currentTimelineIndex++;
+        if (currentTimelineIndex >= timeline.Length)
+        {
+            Debug.LogWarning("더 이상 재생할 타임라인이 없습니다.");
+            return;
+        }
         StartCoroutine(TimeLineCoroutine(timeline[currentTimelineIndex]));
     }
 
-    private IEnumerator TimeLineCoroutine(PlayableDirector timeline, Action onCompleted = null)
+    private IEnumerator TimeLineCoroutine(PlayableDirector timeline)
     {
+        if (timeline == null)
+        {
+            Debug.LogError("타임라인이 null입니다.");
+            yield break;
+        }
+
         MainController.instance.ChangeWaitState();
 
         timeline.Play();
-        yield return new WaitUntil(() => timeline.state == PlayState.Paused);
-        onCompleted?.Invoke();
+        yield return new WaitUntil(() => timeline == null || timeline.state == PlayState.Paused);
+        
+        if(timeline != null)
         DialogueManager.instance.isTransition = true;
     }
-
 }
