5a4f31ddd31b602de98f33398ff4059c68d70726
meo-young <meoyoung000@naver.com>
2025-05-18 23:30:46 +0900

    [Update] 칸나 타임라인 2배

--- Assets/02. Script/Dialogue/Dialogue.cs
+++ Assets/02. Script/Dialogue/Dialogue.cs
@@ -3,11 +3,33 @@ using System.Collections;
 using System;
 
 
-public class Dialogue : MonoBehaviour, IInteractable
+public class Dialogue : MonoBehaviour, IInteractable, IInteraction
 {
+    private GameObject arrow;
+    
+    protected void Awake()
+    {
+        arrow = transform.GetChild(0).gameObject;
+        
+        if (arrow.activeSelf)
+        {
+            arrow.SetActive(false);   
+        }
+    }
+
     public void Interact()
     {
         DialogueManager.instance.ShowDialogue();
         Destroy(this);
+    }
+    
+    public void CanInteraction()
+    {
+        arrow.SetActive(true);
+    }
+
+    public void StopInteraction()
+    {
+        arrow.SetActive(false);
     }
 }
